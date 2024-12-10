4cd333938433affda83ce3cd85def5f72af7b9a5
JuniorG <andresjunior24048hehe@gmail.com>
2024-12-10 17:14:03 -0400

    backend pendiente revisar monto kpi

--- src/main/java/com/medilink/kpi/repositories/CriterioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.medilink.kpi.repositories;
-
-import com.medilink.kpi.entities.Criterio;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CriterioRepository extends JpaRepository<Criterio, Integer> {
-}
